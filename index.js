35f1a5ec1077ac9ca543dc25f20811fe8cef36df
cvirgili <cristiano.virgili@gmail.com>
2017-12-11 17:46:11 +0100

    ok

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
-var socket = require('socket.io-client')('http://localhost');
+var fs = require('fs');
+
+const settings = JSON.parse(fs.readFileSync(__dirname + '/settings.json', 'utf8'));
+var socket = require('socket.io-client')('http://' + settings.host + ':' + settings.port);
 var Omx = require('node-omxplayer');
+var player = Omx();
+
+var deltime, action;
+
+socket.on('connect', () => {
+    player.newSource(settings.mediapath + 'test.mp4');
+    player.pause();
+    socket.emit('readytoplay', settings.id);
+});
+
+socket.on('timesync', (obj) => {
+    deltime = obj.time;
+    action = obj.action;
+});
+
+socket.on('gettime', (t) => {
+    if ((deltime - t) < 0) {
+        switch (action) {
+            case "play":
+                player.play();
+                break;
+            case "pause":
+                player.pause();
+                break;
+        }
+        socket.emit('videostarted');
+    }
+});
