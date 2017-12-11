0ec4d2e73ac8aff0d9287381f9a4a4fa0bc417c5
cvirgili <cristiano.virgili@gmail.com>
2017-12-12 00:12:56 +0100

    ok

--- index.js
+++ index.js
@@ -1,33 +1,69 @@
-var fs = require('fs');
-
+const fs = require('fs');
 const settings = JSON.parse(fs.readFileSync(__dirname + '/settings.json', 'utf8'));
-var socket = require('socket.io-client')('http://' + settings.host + ':' + settings.port);
-var Omx = require('node-omxplayer');
-var player = Omx();
+console.log(JSON.stringify(settings,null,1));
 
-var deltime, action;
+const Omx = require('node-omxplayer');
+const player = Omx('--blank');
+const socket = require('socket.io-client')('http://' + settings.host + ':' + settings.port);
+
+var deltime, action,isgettime=false;
+
+player.on('error',(err)=>{console.log('PLAYER ERROR: '+err);});
 
 socket.on('connect', () => {
-    player.newSource(settings.mediapath + 'test.mp4');
+    console.log('connected');
+});
+
+socket.on('setvideo',(videopath)=>{
+    player.newSource(
+        'http://' + settings.host + ':' + settings.port+'/static/'+videopath+'/'+settings.id+'.mp4',
+        'hdmi',
+        false,
+        1,
+        128
+    );
     player.pause();
-    socket.emit('readytoplay', settings.id);
+    socket.emit('ready',settings.id);
+    socket.emit('readytoplay',settings.id);
 });
 
 socket.on('timesync', (obj) => {
+    isgettime=true;
     deltime = obj.time;
     action = obj.action;
 });
 
+socket.on('stop',()=>{
+    player.newSource('--blank');
+    console.log('\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ STOP');
+});
+
 socket.on('gettime', (t) => {
+    if(!isgettime) return;
     if ((deltime - t) < 0) {
+        isgettime=false;
+        socket.emit('videostarted');
         switch (action) {
             case "play":
                 player.play();
+                console.log('\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ PLAY');
                 break;
             case "pause":
                 player.pause();
+                console.log('\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ PAUSE');
                 break;
         }
-        socket.emit('videostarted');
     }
+});
+socket.on('close',()=>{
+    player.quit();
+});
+socket.on('error',()=>{
+    player.quit();
+});
+socket.on('disconnect',()=>{
+    socket.close();
+});
+socket.on('reload',()=>{
+    player.newSource('--blank');
 });
